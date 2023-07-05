baf094e6e2a46f9e81a0ee7ec498c1e42565baba
saduakassov daniyar <dinovko@gmail.com>
2023-07-05 14:49:13 +0600

    dwnld

---
+++ dwnld.bash
@@ -0,0 +1,7 @@
+cd /opt
+mkdir admin
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz
+wget https://github.com/geoserver/geoserver/archive/2.16.2.zip
+wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.16.2/extensions/geoserver-2.16.2-css-plugin.zip/download
+wget https://github.com/dinovko/water/blob/main/geoserver_data.zip
+wget https://github.com/dinovko/water/blob/main/water-api.war
