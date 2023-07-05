567b88607fa94278f97004470a90a07a750a111a
saduakassov daniyar <dinovko@gmail.com>
2023-07-05 15:35:06 +0600

    -

--- scr.sh
+++ scr.sh
@@ -1,6 +1,6 @@
-yum install wget unzip 
+yum install unzip 
 cd /opt
-mkdir admin2
+mkdir admin
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz
 wget https://github.com/geoserver/geoserver/archive/2.16.2.zip
 wget https://sourceforge.net/projects/geoserver/files/GeoServer/2.16.2/extensions/geoserver-2.16.2-css-plugin.zip/download
